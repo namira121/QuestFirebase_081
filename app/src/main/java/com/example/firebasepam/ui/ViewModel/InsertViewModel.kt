b2ced50a938fc6172b646e5617c6328166e7588f
Namira Pramadhani <namnam6617@gmail.com>
2025-01-12 01:12:18 +0700

    memperbarui state berdasarkan input pengguna

--- app/src/main/java/com/example/firebasepam/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/ViewModel/InsertViewModel.kt
@@ -15,5 +15,12 @@ class InsertViewModel (
     var uiState: FormState by mutableStateOf(FormState.Idle)
         private set
 
+    //Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
 
 }
