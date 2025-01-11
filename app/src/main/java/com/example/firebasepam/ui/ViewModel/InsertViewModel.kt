6973678536dd02aeb6064842f3a5a193c44e9722
Namira Pramadhani <namnam6617@gmail.com>
2025-01-12 02:33:04 +0700

    membuat data class variabel untuk menyimpan data yang diinput

--- app/src/main/java/com/example/firebasepam/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/ViewModel/InsertViewModel.kt
@@ -90,3 +90,13 @@ data class FormErrorState(
                 kelas == null && angkatan == null
     }
 }
+
+//data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
