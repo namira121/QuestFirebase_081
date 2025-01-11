7f6f77ca22e18354ce0513015d7c85f4a58cd8c5
Namira Pramadhani <namnam6617@gmail.com>
2025-01-12 02:53:26 +0700

    membuat composable untuk menjalankan setiap state yang ada

--- app/src/main/java/com/example/firebasepam/ui/View/InsertView.kt
+++ app/src/main/java/com/example/firebasepam/ui/View/InsertView.kt
@@ -2,12 +2,16 @@ package com.example.firebasepam.ui.View
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebasepam.ui.ViewModel.FormState
 import com.example.firebasepam.ui.ViewModel.InsertViewModel
 import com.example.firebasepam.ui.ViewModel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun InsertMhsView(
@@ -20,4 +24,22 @@ fun InsertMhsView(
     val eiEvent = viewModel.uiEvent // state untuk form dan validasii
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
+    //Observasi perubagan state untuks snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when(uiState){
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState. Success, navigate to home "
+                            + uiState.message)
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)//tampilkan snackbar
+                }
+                delay(700)
+                onNavigate()
+
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
 }
