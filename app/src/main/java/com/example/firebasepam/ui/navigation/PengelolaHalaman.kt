e50c6a32e9c0b7784060388a7f743dcfd877ce46
Namira Pramadhani <namnam6617@gmail.com>
2025-01-12 09:21:13 +0700

    import navhost dan membuat route untuk home

---
+++ app/src/main/java/com/example/firebasepam/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,29 @@
+package com.example.firebasepam.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebasepam.ui.View.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinsiInsert.route)
+                },
+            )
+        }
+    }
+}
