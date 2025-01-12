f8c2b23c29240d87f7d7a3df809465faf4af9cdf
Namira Pramadhani <namnam6617@gmail.com>
2025-01-12 08:32:30 +0700

    mambuat Scaffold dan memasukkan customwidget yang sudah dibuat dan insertBodyMhs sebagai body dari halaman insert

--- app/src/main/java/com/example/firebasepam/ui/View/InsertView.kt
+++ app/src/main/java/com/example/firebasepam/ui/View/InsertView.kt
@@ -1,11 +1,21 @@
 package com.example.firebasepam.ui.View
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebasepam.ui.ViewModel.FormState
 import com.example.firebasepam.ui.ViewModel.InsertViewModel
@@ -13,6 +23,7 @@ import com.example.firebasepam.ui.ViewModel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InsertMhsView(
     onBack: () -> Unit,
@@ -45,6 +56,42 @@ fun InsertMhsView(
                     snackbarHostState.showSnackbar(uiState.message)
                 }
             }
+            else -> Unit
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title =  {Text("Tambah Mahasiswa")},
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("back")
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyMhs(
+                uiState = uiEvent,
+                homeUiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFields()){
+                        viewModel.insertMhs()
+                    }
+                }
+            )
         }
     }
 }
