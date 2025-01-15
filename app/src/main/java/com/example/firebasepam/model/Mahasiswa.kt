053483e9a40c245802797732532eb3a4b7bb1fab
Namira Pramadhani <namnam6617@gmail.com>
2025-01-15 12:43:20 +0700

    menambahkan atribut judul, dosen1, dosen2

--- app/src/main/java/com/example/firebasepam/model/Mahasiswa.kt
+++ app/src/main/java/com/example/firebasepam/model/Mahasiswa.kt
@@ -6,7 +6,10 @@ data class Mahasiswa (
     val alamat: String,
     val jenisKelamin: String,
     val kelas: String,
-    val angkatan: String
+    val angkatan: String,
+    val judul: String,
+    val dosen1: String,
+    val dosen2: String
 ){
-    constructor():this("","","","","","")
+    constructor():this("","","","","","", "","","")
 }
